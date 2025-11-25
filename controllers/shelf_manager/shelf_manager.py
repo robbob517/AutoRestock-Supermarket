e8830c0aab31da98b5192a55003d1de5553fafb2
Robert Choi <jd157a69@gmail.com>
2025-11-25 15:26:47 +0000

    Implemented automatic shelf placement

--- controllers/shelf_manager/shelf_manager.py
+++ controllers/shelf_manager/shelf_manager.py
@@ -2,31 +2,49 @@
 
 # You may need to import some classes of the controller module. Ex:
 #  from controller import Robot, Motor, DistanceSensor
-from controller import Robot
+from controller import Supervisor
 
-# create the Robot instance.
-robot = Robot()
 
-# get the time step of the current world.
-timestep = int(robot.getBasicTimeStep())
+# Shelf Dimensions
+SHELF_WIDTH = 2
+SHELF_DEPTH = 0.5
+SHELF_SPACING = 0.01
 
-# You should insert a getDevice-like function in order to get the
-# instance of a device of the robot. Something like:
-#  motor = robot.getDevice('motorname')
-#  ds = robot.getDevice('dsname')
-#  ds.enable(timestep)
+# Aisle Dimensions
+ROWS = 14
+COLUMNS = 10
+AISLE_WIDTH = 2.5
 
-# Main loop:
-# - perform simulation steps until Webots is stopping the controller
-while robot.step(timestep) != -1:
-    # Read the sensors:
-    # Enter here functions to read sensor data, like:
-    #  val = ds.getValue()
+START_X = -11
+START_Y = -10
 
-    # Process sensor data here.
+supervisor = Supervisor()
 
-    # Enter here functions to send actuator commands, like:
-    #  motor.setPosition(10.0)
-    pass
+root_node = supervisor.getRoot()
+children = root_node.getField("children")
+shelves = root_node.
 
-# Enter here exit cleanup code.
+east_facing = True
+for row in range(ROWS):
+    if row % 2 == 0:
+        east_facing = True
+    for col in range(COLUMNS):
+        current_x = START_X + (row * SHELF_DEPTH) + ((row//2) * AISLE_WIDTH)
+        current_y = START_Y + (col * (SHELF_WIDTH + SHELF_SPACING))
+
+        if east_facing:
+            shelf = (f'Shelf {{ '
+                    f'translation {current_x} {current_y} 0 '
+                    f'rotation -0.5773451 0.577351 0.577351 -2.0944 '
+                    f'name "shelf_{row}_{col}"'
+                    f' }} ')
+        else:
+            shelf = (f'Shelf {{ '
+                    f'translation {current_x} {current_y} 0 '
+                    f'name "shelf_{row}_{col}"'
+                    f' }} ')
+
+        children.importMFNodeFromString(-1, shelf)
+    east_facing = False
+
+print("Shelves placed")
