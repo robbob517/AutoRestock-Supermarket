a71bb254a86f2fd9fdd2a93badff0f080991180f
Robert Choi <jd157a69@gmail.com>
2025-12-08 17:02:04 +0000

    Comments + formatting

--- controllers/supermarket_manager/supermarket_manager.py
+++ controllers/supermarket_manager/supermarket_manager.py
@@ -29,49 +29,49 @@ PLACEHOLDER_PRODUCT = {
     }
 
 PRODUCTS = {
-        "BREAD": {"type": "DRY"},
-        "CEREAL": {"type": "DRY"},
-        "WATER": {"type": "DRY"},
-        "CHIPS": {"type": "DRY"},
-        "PASTA": {"type": "DRY"},
+        "BREAD" : {"type" : "DRY"},
+        "CEREAL" : {"type" : "DRY"},
+        "WATER" : {"type" : "DRY"},
+        "CHIPS" : {"type" : "DRY"},
+        "PASTA" : {"type" : "DRY"},
         "KIDNEY BEANS" : {"type" : "DRY"},
         "CHICKPEAS" : {"type" : "DRY"},
         "BAKED BEANS" : {"type" : "DRY"},
         "SOY BEANS" : {"type" : "DRY"},
         "BLACK BEANS" : {"type" : "DRY"},
 
-        "MILK": {"type": "PRODUCE"},
-        "APPLES": {"type": "PRODUCE"},
-        "BANANAS": {"type": "PRODUCE"},
-        "EGGS": {"type": "PRODUCE"},
-        "YOGURT": {"type": "PRODUCE"},
-        "KIWI": {"type": "PRODUCE"},
-        "STRAWBERRY": {"type": "PRODUCE"},
-        "MANGO": {"type": "PRODUCE"},
-        "BLUEBERRY": {"type": "PRODUCE"},
-        "CREAM": {"type": "PRODUCE"},
+        "MILK" : {"type" : "PRODUCE"},
+        "APPLES" : {"type" : "PRODUCE"},
+        "BANANAS" : {"type" : "PRODUCE"},
+        "EGGS" : {"type" : "PRODUCE"},
+        "YOGURT" : {"type" : "PRODUCE"},
+        "KIWI" : {"type" : "PRODUCE"},
+        "STRAWBERRY" : {"type" : "PRODUCE"},
+        "MANGO" : {"type" : "PRODUCE"},
+        "BLUEBERRY" : {"type" : "PRODUCE"},
+        "CREAM" : {"type" : "PRODUCE"},
 
-        "ICE_CREAM": {"type": "FROZEN"},
-        "PIZZA": {"type": "FROZEN"},
-        "CHICKEN": {"type": "FROZEN"},
-        "PEAS": {"type": "FROZEN"},
-        "ICE LOLLY": {"type": "FROZEN"},
-        "HASH BROWN": {"type": "FROZEN"},
-        "TURKEY": {"type": "FROZEN"},
-        "BURGER": {"type": "FROZEN"},
-        "MIXED FRUIT": {"type": "FROZEN"},
+        "ICE_CREAM" : {"type" : "FROZEN"},
+        "PIZZA" : {"type" : "FROZEN"},
+        "CHICKEN" : {"type" : "FROZEN"},
+        "PEAS" : {"type" : "FROZEN"},
+        "ICE LOLLY" : {"type" : "FROZEN"},
+        "HASH BROWN" : {"type" : "FROZEN"},
+        "TURKEY" : {"type" : "FROZEN"},
+        "BURGER" : {"type" : "FROZEN"},
+        "MIXED FRUIT" : {"type" : "FROZEN"},
 
-        "BATH TOWEL": {"type": "NON_FOOD"},
-        "FACE TOWEL": {"type": "NON_FOOD"},
-        "HAIR TOWEL": {"type": "NON_FOOD"},
-        "HAND TOWEL": {"type": "NON_FOOD"},
-        "SHAMPOO": {"type": "NON_FOOD"},
-        "CONDITIONER": {"type": "NON_FOOD"},
-        "BODY WASH": {"type": "NON_FOOD"},
-        "TOOTHBRUSH": {"type": "NON_FOOD"},
-        "TOOTHPASTE": {"type": "NON_FOOD"},
-        "TOILET ROLL": {"type": "NON_FOOD"},
-        "SOAP": {"type": "NON_FOOD"},
+        "BATH TOWEL" : {"type" : "NON_FOOD"},
+        "FACE TOWEL" : {"type" : "NON_FOOD"},
+        "HAIR TOWEL" : {"type" : "NON_FOOD"},
+        "HAND TOWEL" : {"type" : "NON_FOOD"},
+        "SHAMPOO" : {"type" : "NON_FOOD"},
+        "CONDITIONER" : {"type" : "NON_FOOD"},
+        "BODY WASH" : {"type" : "NON_FOOD"},
+        "TOOTHBRUSH" : {"type" : "NON_FOOD"},
+        "TOOTHPASTE" : {"type" : "NON_FOOD"},
+        "TOILET ROLL" : {"type" : "NON_FOOD"},
+        "SOAP" : {"type" : "NON_FOOD"},
 }
 
 SHELF_LEVELS = []
@@ -133,34 +133,37 @@ def product_placement(shelf_x, shelf_y, shelf_col, shelf_row, east_facing):
         product = random.choice((list)(PRODUCTS.keys()))
 
         item_spacing = 0.3
-        fullness = 0.8 # Chance of product being placed
+        fullness = 0.2 # Chance of product being placed
 
-        product_w = PLACEHOLDER_PRODUCT["w"]
+        product_w = PLACEHOLDER_PRODUCT["w"] # Width of product
 
         items_per_row = 5
         shelf_num = 0
 
-        if empty_slots.get((shelf_row, shelf_col)) is None:
+        if empty_slots.get((shelf_row, shelf_col)) is None: # Initialise dict for current shelf
             empty_slots[(shelf_row, shelf_col)] = {"product_type" : { "name" : product, "type" : PRODUCTS[product]["type"] }, "empty_positions" : []}
 
         for z_level in SHELF_LEVELS:
-            index = 0
+            item_index = 0
             shelf_num %= SHELF_COUNT
-            z_level += 0.1
+
+            z_level += 0.1 # Raise product slightly to avoid clipping with shelf when loading
+
             for i in range(items_per_row):
 
                 y_offset = (shelf_y - (SHELF_WIDTH / 2) - SHELF_THICKNESS + ((i + 1) * (product_w + item_spacing)))
-                if random.random() > fullness:  # Add item to shelf
+
+                if random.random() > (1 - fullness):  # Likelihood of placing product on shelf
                     if east_facing:
-                        prod = (f'{PLACEHOLDER_PRODUCT["name"]} {{ '
+                        prod = (f'{PLACEHOLDER_PRODUCT["name"]} {{ '    # Use placeholder item as product model
                                 f'translation {shelf_x} {y_offset} {z_level} '
                                 f'rotation 0 0 1 3.14159 '
-                                f'name "{product}_{shelf_row}_{shelf_col}_{shelf_num}_{index}" '
+                                f'name "{product}_{shelf_row}_{shelf_col}_{shelf_num}_{item_index}" '
                                 f' }} ')
                     else:
                         prod = (f'{PLACEHOLDER_PRODUCT["name"]} {{ '
                                 f'translation {shelf_x} {y_offset} {z_level} '
-                                f'name "{product}_{shelf_row}_{shelf_col}_{shelf_num}_{index}" '
+                                f'name "{product}_{shelf_row}_{shelf_col}_{shelf_num}_{item_index}" '
                                 f' }} ')
 
                     product_children.importMFNodeFromString(-1, prod)
@@ -172,8 +175,7 @@ def product_placement(shelf_x, shelf_y, shelf_col, shelf_row, east_facing):
                     product_position = (shelf_x, y_offset, z_level)
                     empty_slots[(shelf_row, shelf_col)]["empty_positions"].append(product_position)
 
-                index += 1
-
+                item_index += 1
 
             shelf_num += 1
 
